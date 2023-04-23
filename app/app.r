21115074f646dd05a5abfc234cba70248261b203
StellAuror <michalss07@gmail.com>
2023-04-23 23:03:58 +0200

    System browser

---
+++ app/app.r
@@ -0,0 +1,75 @@
+# How to import packages listed in req.txt
+req <- scan(file.path(dirname(getwd()), "req.txt"), character(), quiet = T)
+invisible(lapply(req, library, character.only = T))
+
+library(shiny)
+library(shinyWidgets)
+library(shinyalert)
+library(htmlwidgets)
+library(bs4Dash)
+library(waiter)
+library(tidyverse)
+library(echarts4r)
+library(plotly)
+library(lubridate)
+library(DT)
+library(reactable)
+library(reactablefmtr)
+
+shinyApp(
+  ui = bs4DashPage(
+    title = "3Art",
+    fullscreen = T,
+    scrollToTop = T,
+    dark = NULL,
+#### BODY ####
+    body = bs4DashBody(
+      includeCSS("page.css"),
+      tabItems(
+        tabItem(
+          tabName = "newt",
+          renderUI("newt.datebar")
+        )
+      )
+    ),
+#### SIDEBAR ####
+    sidebar = bs4DashSidebar(
+      collapsed = T,
+      minified = T,
+      elevation = 5,
+      fixed = T,
+      expandOnHover = F,
+      sidebarMenu(
+        id = "sidebar.main",
+        menuItem(
+          text = "New Training", tabName = "newt",
+          icon = icon("compass")
+        ),
+        menuItem(
+          text = "Data", tabName = "data",
+          icon = icon("cogs"),
+          badgeLabel = "loaded",
+          badgeColor = "success"
+        ),
+        menuItem(
+          text = "Filter", tabName = "filter",
+          icon = icon("filter")
+        )
+      )
+    ),
+#### HEADER ####
+    header = bs4DashNavbar(
+      
+    ),
+#### CONTROLBAR ####
+    control = bs4DashControlbar(
+      
+    )
+  ),
+#### SERVER ####
+  server = function(output, input, session) {
+    session$onSessionEnded(function() {
+      stopApp()
+    })
+  } 
+)
